e823d992af69fc1989687d5609b28d063aab7b4a
Adel <adelbables@gmail.com>
2023-09-11 16:17:04 +0200

    build command line tool using cli

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -5,6 +5,8 @@ import (
 	"AALBox/src/view"
 	"bufio"
 	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
 	"os"
 	"strings"
 )
@@ -20,35 +22,47 @@ func NewController() Controller {
 	}
 }
 
-func (c *Controller) Start() {
-	for {
-		fmt.Println("1. Enter a word to play a song")
-		fmt.Println("2. Add a new song to the database")
-		fmt.Print("Choose an option: ")
-		choice := getInput()
-		switch choice {
-		case "1":
-			word := getInputWord()
-			songPath := c.SongRepo.GetSongPath(word)
-			if songPath != "" {
-				view.PlaySong(songPath)
-			} else {
-				view.DisplayMessage("Song not found for the given word.")
-			}
-		case "2":
-			fmt.Print("Enter the word for the song: ")
-			word := getInput()
-			fmt.Print("Enter the path for the song: ")
-			path := getInput()
-			err := c.SongRepo.AddSong(word, path)
-			if err != nil {
-				view.DisplayMessage("Error adding song: " + err.Error())
-			} else {
-				view.DisplayMessage("Song added successfully!")
-			}
-		default:
-			view.DisplayMessage("Invalid option. Please try again.")
-		}
+func (control *Controller) Start() {
+
+	app := &cli.App{
+		Name:  "AALBox",
+		Usage: "Manage and play songs",
+		Commands: []*cli.Command{
+			{
+				Name:  "play",
+				Usage: "Play a song by word",
+				Action: func(c *cli.Context) error {
+					word := c.Args().First()
+					songPath := control.SongRepo.GetSongPath(word)
+					if songPath != "" {
+						view.PlaySong(songPath)
+					} else {
+						fmt.Println("Song not found for the given word.")
+					}
+					return nil
+				},
+			},
+			{
+				Name:  "add",
+				Usage: "Add a new song to the database",
+				Action: func(c *cli.Context) error {
+					word := c.Args().Get(0)
+					path := c.Args().Get(1)
+					err := control.SongRepo.AddSong(word, path)
+					if err != nil {
+						fmt.Println("Error adding song:", err)
+					} else {
+						fmt.Println("Song added successfully!")
+					}
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
