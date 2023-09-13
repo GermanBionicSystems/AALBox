7a893a03bc2a19dd0ca2b9761f8d2d54f650f9b9
Adel <adelbables@gmail.com>
2023-09-13 16:43:22 +0200

    add functionality of stopping a playing song when new tag is scanned

--- src/view/view.go
+++ src/view/view.go
@@ -4,14 +4,42 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
+)
+
+var (
+	currentCmd *exec.Cmd
+	mu         sync.Mutex
 )
 
 func PlaySong(songPath string) {
-	cmd := exec.Command("mpg123", songPath)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	StopSong()
+
+	mu.Lock()
+	currentCmd = exec.Command("mpg123", songPath)
+	currentCmd.Stdout = os.Stdout
+	currentCmd.Stderr = os.Stderr
+	mu.Unlock()
+
+	err := currentCmd.Start()
 	if err != nil {
 		fmt.Println("Error playing song:", err)
+		return
 	}
+	// Wait for the command to finish
+	if err := currentCmd.Wait(); err != nil {
+		fmt.Println("Error playing song:", err)
+	}
+}
+
+func StopSong() {
+	mu.Lock()
+	defer mu.Unlock()
+	if currentCmd != nil && currentCmd.Process != nil {
+		if err := currentCmd.Process.Kill(); err != nil {
+			fmt.Println("Failed to stop song:", err)
+		}
+		currentCmd = nil
+	}
+
 }
